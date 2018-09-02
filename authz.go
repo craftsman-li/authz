f1cbfd119729fbc4dc0c6d46302f7fed49141be6
李万绪 <lwx@terminus.io>
2018-09-02 14:16:30 +0800

    authz

---
+++ authz.go
@@ -0,0 +1,121 @@
+package authz
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/casbin/casbin"
+	"github.com/craftsman-li/kit-wrapper/di"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/dgrijalva/jwt-go/request"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Config struct {
+	SecretKey   string `cfg:"secret_key"`
+	UserInfoKey string `cfg:"user_info_key"`
+}
+
+// 可以由外部解析生成
+//      type A struct {
+//          AuthzConfig *authz.Config `cfg:"authz_config"`
+//      }
+//      authz.AUthzConfig = a.AuthzConfig
+var AuthzConfig *Config
+
+func init() {
+	AuthzConfig = &Config{
+		SecretKey:   "authz.,",
+		UserInfoKey: "user.info.session",
+	}
+	di.Register("authz_config", AuthzConfig)
+}
+
+// NewAuthorizer returns the authorizer, uses a Casbin enforcer as input
+func NewAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		a := &BasicAuthorizer{enforcer: e}
+
+		if permission, login := a.CheckPermission(c); !permission && !login {
+			a.RequirePermission(c.Writer)
+		}
+	}
+}
+
+// BasicAuthorizer stores the casbin handler
+type BasicAuthorizer struct {
+	enforcer *casbin.Enforcer
+}
+
+type AuthzClaims struct {
+	*jwt.StandardClaims
+	TokenType string
+	CustomerInfo
+}
+
+type CustomerInfo struct {
+	Id       string
+	UserName string
+	Email    string
+}
+
+// GetUserName gets the user name from the request.
+// Currently, only HTTP basic authentication is supported
+func (a *BasicAuthorizer) GetUserName(c *gin.Context) string {
+	if nil == AuthzConfig {
+		logrus.Fatalln("authz config not init.")
+		return defaultUser
+	}
+	session := sessions.Default(c)
+	u := session.Get(AuthzConfig.UserInfoKey)
+
+	if nil == u {
+		// 尝试jwt
+		token, err := request.ParseFromRequest(c.Request, request.AuthorizationHeaderExtractor,
+			func(token *jwt.Token) (interface{}, error) {
+				return []byte(AuthzConfig.SecretKey), nil
+			})
+		if nil != err {
+			a.NeedLogin(c.Writer)
+		}
+		if token.Valid {
+			claims := token.Claims.(AuthzClaims)
+			session.Set(AuthzConfig.UserInfoKey, claims.CustomerInfo)
+			return claims.CustomerInfo.Id
+		} else {
+			a.NeedLogin(c.Writer)
+		}
+	} else {
+		return u.(CustomerInfo).Id
+	}
+
+	return defaultUser
+
+}
+
+const defaultUser = "______......-------1024"
+
+// CheckPermission checks the user/method/path combination from the request.
+// Returns true (permission granted) or false (permission forbidden)
+func (a *BasicAuthorizer) CheckPermission(c *gin.Context) (bool, bool) {
+	r := c.Request
+	user := a.GetUserName(c)
+	if user == defaultUser {
+		return false, true
+	}
+	method := r.Method
+	path := r.URL.Path
+	return a.enforcer.Enforce(user, path, method), false
+}
+
+// RequirePermission returns the 403 Forbidden to the client
+func (a *BasicAuthorizer) RequirePermission(w http.ResponseWriter) {
+	w.WriteHeader(403)
+	w.Write([]byte("403 Forbidden\n"))
+}
+
+// RequirePermission returns the 401 Unauthorized to the client
+func (a *BasicAuthorizer) NeedLogin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte("403 Forbidden\n"))
+}
---
+++ go.mod
@@ -0,0 +1,32 @@
+module authz
+
+require (
+	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
+	github.com/Sirupsen/logrus v1.0.6
+	github.com/casbin/casbin v1.6.0
+	github.com/casbin/gorm-adapter v0.0.0-20171006093545-e56c6daebd5e // indirect
+	github.com/craftsman-li/biz v0.0.0-20180901123855-c406ca9422ee // indirect
+	github.com/craftsman-li/kit-wrapper v0.0.0-20180826092309-e736458d3bff
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/sessions v0.0.0-20180724132311-854e10e72056
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/gorilla/sessions v1.1.1 // indirect
+	github.com/jinzhu/gorm v1.9.1 // indirect
+	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/jtolds/gls v4.2.1+incompatible // indirect
+	github.com/lib/pq v1.0.0 // indirect
+	github.com/mattn/go-isatty v0.0.3 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/sirupsen/logrus v1.0.6 // indirect
+	github.com/smartystreets/assertions v0.0.0-20180820201707-7c9eb446e3cf // indirect
+	github.com/smartystreets/goconvey v0.0.0-20180222194500-ef6db91d284a // indirect
+	github.com/ugorji/go v1.1.1 // indirect
+	golang.org/x/sys v0.0.0-20180824143301-4910a1d54f87 // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+)
+
+replace golang.org/x/sys/unix v0.0.0 => github.com/golang/sys/unix v0.0.0
